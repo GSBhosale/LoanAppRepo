ad316f592b94aed4a224d05e38ce433f4dcbc5de
GSBhosale <gauravsanjaybhosale@gmail.com>
2023-07-10 15:30:00 +0530

    initial comit

---
+++ LoanApplocation/src/main/java/com/bitlogic/LoanApplocation/LoanApplocationApplication.java
@@ -0,0 +1,13 @@
+package com.bitlogic.LoanApplocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApplocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanApplocationApplication.class, args);
+	}
+
+}
---
+++ LoanApplocation/src/test/java/com/bitlogic/LoanApplocation/LoanApplocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitlogic.LoanApplocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApplocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
